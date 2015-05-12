6e71206b28aedb497a3d56e67a2d099c32ceb310
Nayuki Minase <nayuki@eigenstate.org>
2015-05-12 19:16:06 +0000

    Changed JavaScript syntax to not avoid the less-than symbol.

--- clock.js
+++ clock.js
@@ -10,9 +10,9 @@ var curFontSize = 24;  // Do not change
 function updateClock() {
 	var d = new Date();
 	var s = "";
-	s += (10 > d.getHours  () ? "0" : "") + d.getHours  () + ":";
-	s += (10 > d.getMinutes() ? "0" : "") + d.getMinutes() + ":";
-	s += (10 > d.getSeconds() ? "0" : "") + d.getSeconds();
+	s += (d.getHours  () < 10 ? "0" : "") + d.getHours  () + ":";
+	s += (d.getMinutes() < 10 ? "0" : "") + d.getMinutes() + ":";
+	s += (d.getSeconds() < 10 ? "0" : "") + d.getSeconds();
 	textNode.data = s;
 	setTimeout(updateClock, 1000 - d.getTime() % 1000 + 20);
 }
@@ -20,7 +20,7 @@ function updateClock() {
 
 function updateTextSize() {
 	var targetWidth = 0.9;  // Proportion of full screen width
-	for (var i = 0; 3 > i; i++) {  // Iterate for better better convergence
+	for (var i = 0; i < 3; i++) {  // Iterate for better better convergence
 		var newFontSize = textElem.parentNode.offsetWidth * targetWidth / textElem.offsetWidth * curFontSize;
 		textElem.style.fontSize = newFontSize.toFixed(3) + "pt";
 		curFontSize = newFontSize;
