3f6c9ffc88b11df70e05ae328e3586d8d7af149f
Nayuki Minase <nayuki@eigenstate.org>
2015-05-12 19:04:21 +0000

    Moved CSS and JavaScript code to external files, removed unnecessary comments, adjusted blank lines.

---
+++ clock.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+var textElem = document.getElementById("clocktext");
+var textNode = document.createTextNode("");
+textElem.appendChild(textNode);
+var curFontSize = 24;  // Do not change
+
+
+function updateClock() {
+	var d = new Date();
+	var s = "";
+	s += (10 > d.getHours  () ? "0" : "") + d.getHours  () + ":";
+	s += (10 > d.getMinutes() ? "0" : "") + d.getMinutes() + ":";
+	s += (10 > d.getSeconds() ? "0" : "") + d.getSeconds();
+	textNode.data = s;
+	setTimeout(updateClock, 1000 - d.getTime() % 1000 + 20);
+}
+
+
+function updateTextSize() {
+	var targetWidth = 0.9;  // Proportion of full screen width
+	for (var i = 0; 3 > i; i++) {  // Iterate for better better convergence
+		var newFontSize = textElem.parentNode.offsetWidth * targetWidth / textElem.offsetWidth * curFontSize;
+		textElem.style.fontSize = newFontSize.toFixed(3) + "pt";
+		curFontSize = newFontSize;
+	}
+}
+
+
+updateClock();
+updateTextSize();
+window.addEventListener("resize", updateTextSize);
