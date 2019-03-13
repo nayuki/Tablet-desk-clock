e73bd6d1d1f44c61da40cf1a42a301691b54453b
Project Nayuki <me@nayuki.io>
2019-03-13 20:34:58 +0000

    Implemented weather data handling by adding TS, CSS, HTML code.

--- web/clock.ts
+++ web/clock.ts
@@ -57,6 +57,87 @@ namespace clock {
 
 
 
+namespace weather {
+	
+	let initialized: boolean = false;
+	let eraseWeather: number = -1;
+	
+	
+	export async function initialize(): Promise<void> {
+		if (initialized)
+			throw "Assertion error";
+		initialized = true;
+		
+		while (true) {
+			await updateWeatherOnce();
+			
+			// Schedule next update at about 5 minutes past the hour
+			const now = new Date();
+			let next = new Date(now.getTime());
+			next.setMinutes(4);
+			next.setSeconds(0);
+			next.setMilliseconds(Math.random() * 2 * 60 * 1000);  // Jitter 2 minutes
+			while (next.getTime() < now.getTime() + 5 * 60 * 1000)
+				next.setTime(next.getTime() + 60 * 60 * 1000);
+			await util.sleep(next.getTime() - now.getTime());
+		}
+	}
+	
+	
+	async function updateWeatherOnce(): Promise<void> {
+		if (eraseWeather != -1)
+			clearTimeout(eraseWeather);
+		eraseWeather = setTimeout(() => {
+				util.getElem("clock-weather-description").textContent = "";
+				util.getElem("clock-weather-temperature").textContent = "";
+				eraseWeather = -1;
+			}, 30000);
+		
+		for (let i = 0; i < 5; i++) {
+			try {
+				await tryUpdateWeather();
+				clearTimeout(eraseWeather);
+				eraseWeather = -1;
+				break;
+			} catch (e) {
+				await util.sleep(Math.pow(4, i + 1) * 1000);
+			}
+		}
+	}
+	
+	
+	async function tryUpdateWeather(): Promise<void> {
+		if (util.configuration === null)
+			throw "Configuration missing";
+		const xhr = await util.doXhr("/proxy/" + encodeURIComponent(util.configuration["weather-canada-url"]), "document", 15000);
+		if (xhr.status != 200)
+			throw "Invalid status";
+		let data = xhr.response;
+		if (!(data instanceof Document))
+			throw "Invalid type";
+		
+		function getText(selector: string): string {
+			const node: Element|null = data.querySelector(selector);
+			if (node === null)
+				throw "Node missing";
+			const text: string|null = node.textContent;
+			if (text === null)
+				throw "Text missing";
+			return text;
+		}
+		
+		util.getElem("clock-weather-description").textContent = getText("siteData > currentConditions > condition");
+		const temperStr = getText("siteData > currentConditions > temperature");
+		util.getElem("clock-weather-temperature").textContent = Math.round(parseFloat(temperStr)) + " " + DEGREE + "C";
+	}
+	
+	
+	const DEGREE = "\u00B0";
+	
+}
+
+
+
 namespace util {
 	
 	export let configuration: any = null;
@@ -84,8 +165,14 @@ namespace util {
 	}
 	
 	
+	export function sleep(millis: number): Promise<void> {
+		return new Promise(resolve => setTimeout(resolve, millis));
+	}
+	
+	
 	async function initialize() {
 		configuration = (await doXhr("config.json", "json", 60000)).response;
+		weather.initialize();
 	}
 	
 	
